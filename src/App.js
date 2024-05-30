0b4343da8d6868db90e1fc7650c889b4a54d90d0
Umesh gardas <umeshgardas123@gmail.com>
2024-05-30 23:21:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,337 @@
+import './App.css';
+import styles from "./FasterTemplate.module.css";
+
+function App() {
+  return (
+    <div className={styles.fasterTemplate}>
+      <div className={styles.fasterTemplateChild} />
+      <img
+        className={styles.fasterTemplateItem}
+        alt=""
+        src="/group-27169.svg"
+      />
+      <img className={styles.maskGroupIcon} alt="" src="/mask-group@2x.png" />
+      <img
+        className={styles.untitled21536x73521}
+        alt=""
+        src="/untitled21536x735-2-1@2x.png"
+      />
+      <img
+        className={styles.untitled31536x7351Icon}
+        alt=""
+        src="/untitled31536x735-1@2x.png"
+      />
+      <img
+        className={styles.untitled21536x7351Icon}
+        alt=""
+        src="/untitled21536x735-1@2x.png"
+      />
+      <div className={styles.fasterTemplateInner}>
+        <div className={styles.groupParent}>
+          <img className={styles.groupIcon} alt="" src="/group.svg" />
+          <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+          <img className={styles.groupIcon2} alt="" src="/group3.svg" />
+        </div>
+      </div>
+      <div className={styles.fasterTemplateInner}>
+        <div className={styles.groupParent}>
+          <img className={styles.groupIcon} alt="" src="/group.svg" />
+          <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+          <img className={styles.groupIcon2} alt="" src="/group3.svg" />
+        </div>
+      </div>
+      <div className={styles.signIn}>Sign In</div>
+      <img className={styles.logoIcon} alt="" src="/logo3@2x.png" />
+      <div className={styles.fasterTemplateInner}>
+        <div className={styles.groupParent}>
+          <img className={styles.groupIcon} alt="" src="/group.svg" />
+          <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+          <img className={styles.groupIcon2} alt="" src="/group3.svg" />
+        </div>
+      </div>
+      <b className={styles.frequentlyAskedQuestions}>
+        Frequently Asked Questions
+      </b>
+      <b className={styles.letsGetIn}>Let’s Get in Touch</b>
+      <b className={styles.whatWeDo}>What We do</b>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.rectangleParent}>
+        <div className={styles.groupChild} />
+        <div className={styles.groupItem} />
+        <div className={styles.groupInner} />
+        <img
+          className={styles.rectangleIcon}
+          alt=""
+          src="/rectangle-30@2x.png"
+        />
+        <img className={styles.groupChild1} alt="" src="/rectangle-33@2x.png" />
+        <img className={styles.groupChild2} alt="" src="/rectangle-35@2x.png" />
+      </div>
+      <div className={styles.fasterTemplateChild1} />
+      <b className={styles.email}>email</b>
+      <div className={styles.fasterTemplateChild2} />
+      <div className={styles.fasterTemplateChild3} />
+      <b className={styles.name}>name</b>
+      <b className={styles.message}>message</b>
+      <div className={styles.lineDiv} />
+      <div className={styles.fasterTemplateChild4} />
+      <b className={styles.howCanI}> How can I contact Inkyy Team?</b>
+      <img className={styles.frameIcon} alt="" src="/frame.svg" />
+      <div className={styles.youCanReachContainer}>
+        <span>{`You can reach us through our contact form on our website or by emailing us at `}</span>
+        <b className={styles.helloinkyycom}>hello@inkyy.com</b>
+        <span> We typically respond within 24 hours.</span>
+      </div>
+      <div className={styles.fasterTemplateChild5} />
+      <b className={styles.whatServicesDo}>What services do you offer?</b>
+      <img className={styles.frameIcon1} alt="" src="/frame1.svg" />
+      <div className={styles.fasterTemplateChild6} />
+      <b className={styles.doYouProvide}>
+        Do you provide website maintenance services?
+      </b>
+      <img className={styles.frameIcon2} alt="" src="/frame2.svg" />
+      <div className={styles.exampleemailcom}>example@email.com</div>
+      <div className={styles.fasterTemplateChild7} />
+      <b className={styles.howLongDoes}>
+        {" "}
+        How long does it take to design and develop a website?
+      </b>
+      <img className={styles.frameIcon3} alt="" src="/frame3.svg" />
+      <div className={styles.fasterTemplateChild8} />
+      <b className={styles.doYouRequire}>
+        Do you require a deposit for projects?
+      </b>
+      <img className={styles.frameIcon4} alt="" src="/frame4.svg" />
+      <div className={styles.fullName}>full name</div>
+      <div className={styles.writeYourMessage}>write your message....</div>
+      <div className={styles.getInTouchWrapper}>
+        <b className={styles.getInTouch}>Get in Touch</b>
+      </div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild3} />
+        <div className={styles.webDesign}>Web Design</div>
+        <div className={styles.fromConceptTo}>
+          From concept to launch, we create stunning, user-centric websites that
+          elevate your brand and engage your audience.
+        </div>
+        <div className={styles.groupChild4} />
+        <img className={styles.frameIcon5} alt="" src="/frame5.svg" />
+      </div>
+      <img
+        className={styles.untitled21536x73511}
+        alt=""
+        src="/untitled21536x735-1-1@2x.png"
+      />
+      <div className={styles.awesomeUiDarkTemplateForWParent}>
+        <b className={styles.awesomeUiDark}>
+          Awesome UI Dark Template for Webflow Agency
+        </b>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.groupChild5} />
+          <b className={styles.getInTouch1}>Get in Touch</b>
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.groupChild6} />
+        <b className={styles.signUp}>Sign Up</b>
+      </div>
+      <b className={styles.checkOurWork}>Check our Work</b>
+      <div className={styles.takeALook}>
+        Take a look at some of our recent projects to see how we've helped
+        businesses like yours succeed online.
+      </div>
+      <div className={styles.ellipseDiv} />
+      <img className={styles.frameIcon6} alt="" src="/frame6.svg" />
+      <div className={styles.fasterTemplateChild9} />
+      <img className={styles.frameIcon7} alt="" src="/frame7@2x.png" />
+      <div className={styles.fasterTemplateChild10} />
+      <div className={styles.fasterTemplateChild11} />
+      <div className={styles.fasterTemplateChild12} />
+      <b className={styles.pricing}>Pricing</b>
+      <b className={styles.unlimitedServices}>Unlimited Services</b>
+      <b className={styles.letsScheduleA}>Let’s Schedule a Meeting</b>
+      <div className={styles.takeALook1}>
+        Take a look at some of our recent projects to see how we've helped
+        businesses like yours succeed online.
+      </div>
+      <div className={styles.takeALook2}>
+        Take a look at some of our recent projects to see how we've helped
+        businesses like yours succeed online.
+      </div>
+      <b className={styles.b}>$3,250</b>
+      <div className={styles.mo}>/mo</div>
+      <div className={styles.groupParent1}>
+        <div className={styles.unlimitedRequestsParent}>
+          <div className={styles.unlimitedRequests}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+        <div className={styles.unlimitedRequestsGroup}>
+          <div className={styles.unlimitedRequests}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+        <div className={styles.unlimitedRequestsContainer}>
+          <div className={styles.unlimitedRequests}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+      </div>
+      <div className={styles.groupParent2}>
+        <div className={styles.unlimitedRequestsParent}>
+          <div className={styles.unlimitedRequests3}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+        <div className={styles.unlimitedRequestsGroup}>
+          <div className={styles.unlimitedRequests3}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+        <div className={styles.unlimitedRequestsContainer}>
+          <div className={styles.unlimitedRequests3}>Unlimited requests</div>
+          <img className={styles.frameIcon8} alt="" src="/frame8.svg" />
+        </div>
+      </div>
+      <div className={styles.fasterTemplateInner2}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild7} />
+          <b className={styles.scheduleMeeting}>Schedule Meeting</b>
+        </div>
+      </div>
+      <img className={styles.frameIcon14} alt="" src="/frame9.svg" />
+      <div className={styles.basedOn145Container}>
+        <span className={styles.span}>{`5.0 `}</span>
+        <span className={styles.basedOn}>Based on</span>
+        <span className={styles.span}>{` 145 `}</span>
+        <span className={styles.basedOn}>Reviews</span>
+      </div>
+      <div className={styles.starParent}>
+        <img className={styles.starIcon} alt="" src="/star-1.svg" />
+        <img className={styles.groupChild8} alt="" src="/star-2.svg" />
+        <img className={styles.groupChild9} alt="" src="/star-3.svg" />
+        <img className={styles.groupChild10} alt="" src="/star-4.svg" />
+        <img className={styles.groupChild11} alt="" src="/star-5.svg" />
+      </div>
+      <img
+        className={styles.fasterTemplateChild13}
+        alt=""
+        src="/group-13679.svg"
+      />
+      <b className={styles.home}>Home</b>
+      <div className={styles.about}>About</div>
+      <div className={styles.howItWorks}>How it Works</div>
+      <div className={styles.services}>Services</div>
+      <div className={styles.fasterTemplateChild14} />
+      <img className={styles.frameIcon15} alt="" src="/frame10.svg" />
+      <img className={styles.logoIcon1} alt="" src="/logo4@2x.png" />
+      <div className={styles.readyToElevate}>
+        Ready to elevate your online presence? Contact us today to discuss your
+        project and discover how we can bring your vision to life.
+      </div>
+      <div className={styles.madeWithLove}>
+        Made with love powered by inkyy.com
+      </div>
+      <div className={styles.homeParent}>
+        <div className={styles.home1}>Home</div>
+        <div className={styles.about1}>About</div>
+        <div className={styles.howItWorks1}>How it Works</div>
+        <div className={styles.services1}>Services</div>
+      </div>
+      <img
+        className={styles.fasterTemplateChild15}
+        alt=""
+        src="/group-27156.svg"
+      />
+      <img
+        className={styles.fasterTemplateChild16}
+        alt=""
+        src="/group-27155.svg"
+      />
+      <img
+        className={styles.fasterTemplateChild17}
+        alt=""
+        src="/group-27154.svg"
+      />
+      <img
+        className={styles.fasterTemplateChild18}
+        alt=""
+        src="/group-27153.svg"
+      />
+      <div className={styles.fasterTemplateChild19} />
+      <div className={styles.uiuxDesign}>UI/UX Design</div>
+      <div className={styles.fromConceptTo1}>
+        From concept to launch, we create stunning, user-centric websites that
+        elevate your brand and engage your audience.
+      </div>
+      <div className={styles.fasterTemplateInner3}>
+        <div className={styles.groupChild12} />
+      </div>
+      <img className={styles.frameIcon16} alt="" src="/frame11.svg" />
+      <div className={styles.fasterTemplateChild20} />
+      <div className={styles.responsiveDesign}>Responsive Design</div>
+      <div className={styles.fromConceptTo2}>
+        From concept to launch, we create stunning, user-centric websites that
+        elevate your brand and engage your audience.
+      </div>
+      <div className={styles.fasterTemplateInner4}>
+        <div className={styles.groupChild12} />
+      </div>
+      <img className={styles.frameIcon17} alt="" src="/frame12.svg" />
+      <div className={styles.fasterTemplateChild21} />
+      <img
+        className={styles.fasterTemplateChild22}
+        alt=""
+        src="/rectangle-44@2x.png"
+      />
+      <div className={styles.eCommerceSolutions}>E-commerce Solutions:</div>
+      <div className={styles.fromConceptTo3}>
+        From concept to launch, we create stunning, user-centric websites that
+        elevate your brand and engage your audience.
+      </div>
+      <div className={styles.fasterTemplateInner5}>
+        <div className={styles.groupChild12} />
+      </div>
+      <img className={styles.frameIcon18} alt="" src="/frame13.svg" />
+      <div className={styles.fasterTemplateChild23} />
+      <div className={styles.customDevelopment}>Custom Development</div>
+      <div className={styles.fromConceptTo4}>
+        From concept to launch, we create stunning, user-centric websites that
+        elevate your brand and engage your audience.
+      </div>
+      <div className={styles.fasterTemplateInner6}>
+        <div className={styles.groupChild12} />
+      </div>
+      <img className={styles.frameIcon19} alt="" src="/frame14.svg" />
+      <div className={styles.fasterTemplateChild24} />
+      <div className={styles.fasterTemplateChild25} />
+      <div className={styles.webflow}>Webflow</div>
+      <div className={styles.fromConceptTo5}>
+        From concept to launch, we create stunning, user-centric websites that
+        elevate your brand and engage your audience.
+      </div>
+      <img className={styles.vectorIcon} alt="" src="/vector8.svg" />
+      <div className={styles.groupParent3}>
+        <div className={styles.rectangleWrapper}>
+          <div className={styles.groupChild12} />
+        </div>
+        <img className={styles.vectorIcon1} alt="" src="/vector9.svg" />
+      </div>
+    </div>
+  );
+};
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  
+export default App;
